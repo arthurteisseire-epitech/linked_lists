6ba8c2831c08d909c189d0254bc8764d6fd8b11a
arthur <arthur.teisseire@epitech.eu>
2018-12-21 20:52:34 +0100

    Add test for stack_pop

--- stack/tests/src/tests_stack_pop.c
+++ stack/tests/src/tests_stack_pop.c
@@ -8,12 +8,18 @@
 #include <criterion/criterion.h>
 #include "stack.h"
 
+static const char *data1 = "data1";
+static const char *data2 = "data2";
+
 Test(stack_pop, basic)
 {
     stack_t *stack = stack_new();
 
     cr_assert_not_null(stack);
-    cr_assert(stack_push(stack, "data") != -1);
+    cr_assert(stack_push(stack, (void *)data1) != -1);
+    cr_assert(stack_push(stack, (void *)data2) != -1);
+    stack_pop(stack, NULL);
+    cr_assert_str_eq(stack_get(stack), data1);
     stack_pop(stack, NULL);
     cr_assert_null(stack_get(stack));
     free(stack);
